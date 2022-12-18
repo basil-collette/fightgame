aea4543056d0668e3c17497b173098454e25972a
Basouli <113702613+basil-collette@users.noreply.github.com>
2022-12-18 23:15:06 +0100

    update is fps responsive

--- Entity/Sprite.js
+++ Entity/Sprite.js
@@ -41,7 +41,7 @@ export default class Sprite {
 
     update(lastSecondFPS) {
         this.spriteFPS = this.framesMax * this.framesCycle
-        this.framesHold = Math.round(lastSecondFPS / this.spriteFPS);
+        this.framesHold = lastSecondFPS / this.spriteFPS; // = Math.round(lastSecondFPS / this.spriteFPS);
     }
 
     draw(interp) {
@@ -75,7 +75,7 @@ export default class Sprite {
     animateFrame() {
         this.framesElapsed++;
 
-        if (this.framesElapsed % this.framesHold === 0) {
+        if (this.framesElapsed % this.framesHold  < 1) {
             if (this.frameCurrent < this.framesMax - 1) {
                 this.frameCurrent++;
             } else {
@@ -97,75 +97,3 @@ export default class Sprite {
     }
 
 }
-/*
-export default class Sprite {   
-    constructor(
-        {canvas, context},
-        {
-            position,
-            imageSrc,
-            scale = 1,
-            framesMax = 1,
-            offSet = {x: 0, y: 0},
-            mirrored = false
-        }
-    ) {
-        this.canvas = canvas;
-        this.context = context;
-        
-        this.position = position;
-        this.image = new Image();
-        this.image.src = imageSrc;
-        this.scale = scale;
-        this.framesMax = framesMax;
-        this.frameCurrent = 0;
-        this.framesElapsed = 0;
-        this.framesHold = 5;
-        this.offSet = offSet;
-
-        this.mirrored = mirrored;        
-    }
-
-    draw() {
-        const finalWidth = this.image.width / this.framesMax;
-
-        this.context.save();
-
-        this.context.scale(this.mirrored ? -1 : 1, 1);
-
-        this.context.drawImage(
-            this.image,
-            this.frameCurrent * finalWidth,
-            0,
-            finalWidth,
-            this.image.height,
-            this.mirrored ? (finalWidth * this.scale * -1) - (this.position.x - this.offSet.x) : (this.position.x - this.offSet.x),
-            this.position.y - this.offSet.y,
-            finalWidth * this.scale,
-            this.image.height * this.scale
-        );
-
-        //this.context.strokeStyle = 'blue';
-        //this.context.strokeRect(this.mirrored ? (finalWidth * this.scale * -1) - (this.position.x - this.offSet.x) : (this.position.x - this.offSet.x), (this.position.y - this.offSet.y), finalWidth * this.scale, this.image.height * this.scale);
-
-        this.context.restore();
-    }
-
-    animateFrame() {
-        this.framesElapsed++;
-        
-        if(this.framesElapsed % this.framesHold === 0) {
-            if (this.frameCurrent < this.framesMax - 1) {
-                this.frameCurrent++; 
-            } else {
-                this.frameCurrent = 0;
-            }
-        }        
-    }
-
-    update() {
-        this.draw();
-        this.animateFrame();
-    }
-}
-*/
